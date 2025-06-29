a4df943854ed061327a295007aa4dd817cc23c99
19991126kai <19991126kai@gmail.com>
2025-06-30 08:37:45 +0900

    アンフォロー機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
 
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
+    resources :unfollows, only: [:create]
   end
 
   resource :profile, only: [:show, :edit, :update]
